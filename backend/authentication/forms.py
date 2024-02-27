6e951eeb934eb27fa11ed83e233ac7605d9e67e0
Henri Sebille <hsebille@1A2.42angouleme.fr>
2024-02-27 17:47:10 +0100

    started authentication

--- backend/authentication/forms.py
+++ backend/authentication/forms.py
@@ -1,1 +1,6 @@
-# authentication/forms.py
+from django import forms
+
+class LoginForm(forms.Form):
+	email = forms.CharField(max_length=50, label='Email')
+	username = forms.CharField(max_length=12, label='Username')
+	password = forms.CharField(max_length=15, widget=forms.PasswordInput, label='Password')
