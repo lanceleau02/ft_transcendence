d244ad65fd30fd5e61d9011c5ef0515286d38823
lanceleau02 <lancelot.prieure@gmail.com>
2024-03-08 16:03:31 +0100

    Tiens Henri

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -118,6 +118,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../frontend'),
 ]
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
