1579c98a5f6339191d8032361397ed7480ecb092
lanceleau02 <lancelot.prieure@gmail.com>
2024-04-09 14:08:35 +0200

    need to pull

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -21,8 +21,11 @@ def signin(request):
                 else:
                     return render(request, 'index.html', {'form': form})
     else:
+        if request.GET.get('Valid') == "true":
+            form = forms.LoginForm()
+            return render(request, 'views/signin.html', context={'form': form})
         form = forms.LoginForm()
-        return render(request, 'views/signin.html', context={'form': form})
+        return render(request, 'index.html', {'form': form})
 
 def signup(request):
     form = forms.SignupForm()
@@ -30,28 +33,33 @@ def signup(request):
         form = forms.SignupForm(request.POST) 
         if form.is_valid():
             form.save()
-            return redirect('batpong')
+            return redirect('batcave')
         else:
             return render(request, 'index.html')
-    return render(request, 'views/signup.html', context={'form': form})
+    if request.GET.get('Valid') == "true":
+        form = forms.SignupForm() 
+        return render(request, 'views/signup.html', context={'form': form})
+    form = forms.SignupForm() 
+    return render(request, 'index.html', {'form': form})
 
-@login_required
 def send_friend_request(request, userID):
     from_user = request.user
     to_user = User.objects.get(id=userID)
     friend_request, created = Friend_Request.objects.get_or_create(from_user=from_user, to_user=to_user)
     return render (request, 'views/batprofile.html')
 
-@login_required
 def accept_friend_request(request, requestID):
     friend_request = Friend_Request.objects.get(id=requestID)
     friend_request.to_user.friends.add(friend_request.from_user)
     friend_request.from_user.friends.add(friend_request.to_user)
     friend_request.delete()
-    return render (request, 'views/batprofile.html')
+    if request.GET.get('Valid') == "true":
+        return render (request, 'views/batcave.html')
+    return render(request, 'index.html')
 
-@login_required
 def decline_friend_request(request, requestID):
     friend_request = Friend_Request.objects.get(id=requestID)
     friend_request.delete()
-    return render (request, 'views/batprofile.html')
+    if request.GET.get('Valid') == "true":
+        return render (request, 'views/batprofile.html')
+    return render(request, 'index.html')
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -16,47 +16,46 @@ def batcave(request):
 		return render(request, "views/batcave.html", {'user': user})
 	return render(request, "index.html")
 
-@login_required
 def batprofile(request):
-	""" if request.GET.get('Valid') == "true":
-		print("coucou")
-		if request.user.is_authenticated == True:
-			return render(request, "index.html") """
-	user = request.user
-	avatar_url = user.avatar.url if user.avatar else None
-	if request.method == 'POST':
-		formUsername = UpdateUsername(request.POST, instance=request.user)
-		formPassword = CustomPasswordChangeForm(request.user, request.POST)
-		formAvatar = AvatarForm(request.POST, request.FILES)
-		if formUsername.is_valid():
-			formUsername.save()
-			return render(request, 'index.html')
-		if formPassword.is_valid():
-			formPassword.save()
-			return render(request, 'index.html')
-		if formAvatar.is_valid():
-			user.avatar = formAvatar.cleaned_data['avatar']
-			user.save()
-			return render(request, 'index.html')
-	else:
-		formUsername = UpdateUsername()
-		formPassword = CustomPasswordChangeForm(request.user)
-		formAvatar = AvatarForm()
-		all_users = User.objects.exclude(id=request.user.id).exclude(is_superuser=True)
-		all_friend_request = Friend_Request.objects.filter(to_user=user)
-		friends = user.friends.all()
-		if request.GET.get('Valid') == "true":
-			return render(request, "views/batprofile.html", {
-				'formUsername':formUsername, 
-				'user':user,
-				'formPassword':formPassword,
-				'formAvatar':formAvatar,
-				'avatar_url':avatar_url,
-				'all_users':all_users,
-				'all_friend_request':all_friend_request,
-				'friends':friends,
-			})
-		return render(request, "index.html")
+	if request.GET.get('Valid') == "true":
+		if request.user.is_authenticated == False:
+			form = LoginForm()
+			return render(request, 'views/signin.html', context={'form': form})
+		user = request.user
+		avatar_url = user.avatar.url if user.avatar else None
+		if request.method == 'POST':
+			formUsername = UpdateUsername(request.POST, instance=request.user)
+			formPassword = CustomPasswordChangeForm(request.user, request.POST)
+			formAvatar = AvatarForm(request.POST, request.FILES)
+			if formUsername.is_valid():
+				formUsername.save()
+				return render(request, 'index.html')
+			if formPassword.is_valid():
+				formPassword.save()
+				return render(request, 'index.html')
+			if formAvatar.is_valid():
+				user.avatar = formAvatar.cleaned_data['avatar']
+				user.save()
+				return render(request, 'index.html')
+		else:
+			formUsername = UpdateUsername()
+			formPassword = CustomPasswordChangeForm(request.user)
+			formAvatar = AvatarForm()
+			all_users = User.objects.exclude(id=request.user.id).exclude(is_superuser=True)
+			all_friend_request = Friend_Request.objects.filter(to_user=user)
+			friends = user.friends.all()
+			if request.GET.get('Valid') == "true":
+				return render(request, "views/batprofile.html", {
+					'formUsername':formUsername, 
+					'user':user,
+					'formPassword':formPassword,
+					'formAvatar':formAvatar,
+					'avatar_url':avatar_url,
+					'all_users':all_users,
+					'all_friend_request':all_friend_request,
+					'friends':friends,
+				})
+	return render(request, "index.html")
 
 def batpong(request):
 	if request.GET.get('Valid') == "true":
